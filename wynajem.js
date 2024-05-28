33a867946f52922ac47f05acd11937db9b28049c
Kacper Mróz <kacper.mroz1@gmail.com>
2024-05-28 23:26:22 +0200

    naprawienie problemu z tworzeniem podwójnych ogłoszeń, dodanie akordeonów na nowo(tak na prawde to byly juz tylko robily sie poprzez java scripta zeby dane w panelu administracyjnym przekazywaly do nich, rozbudowanie filtracji(dodanie filtracji od roku produkcji (od - do), zmiana tekstu w panelu administratora, gdyż testowałem dodawanie kilku zdjęć naraz i trzeba zdjęcia dodwać po przecinku bez spacji jak to sie robi intuicyjnie, więc dodałem podpis aby zdjęcia dodwać po przecinku bez spacji.

--- wynajem.js
+++ wynajem.js
@@ -1,70 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let offers = JSON.parse(localStorage.getItem('offers')) || [];
-    const offersContainer = document.getElementById('offers-container');
-    
-    offers.forEach((offer, index) => {
-        const offerElement = document.createElement('div');
-        offerElement.className = 'offer';
-        offerElement.setAttribute('data-price', offer.price);
-        offerElement.setAttribute('data-brand', offer.brand);
-        offerElement.setAttribute('data-type', offer.type);
-        
-        const carouselId = 'carousel' + index;
-        
-        offerElement.innerHTML = `
-            <div class="carousel" id="${carouselId}">
-                <button class="prev" onclick="changeSlide(-1, '${carouselId}')">&#10094;</button>
-                ${offer.images.map((img, idx) => `
-                    <img src="${img.trim()}" alt="Oferta ${idx + 1}" class="slide ${idx === 0 ? 'active' : ''}">
-                `).join('')}
-                <button class="next" onclick="changeSlide(1, '${carouselId}')">&#10095;</button>
-            </div>
-            <p class="cena">Cena: ${offer.price}zł + VAT/miesiąc</p>
-            <button class="accordion">OPIS</button>
-            <div class="panel">
-                <p><strong>Marka:</strong> ${offer.brand}</p>
-                <p><strong>Rok produkcji:</strong> ${offer.year}</p>
-                <p><strong>VIN:</strong> ${offer.vin}</p>
-                <p><strong>Typ naczepy:</strong> ${offer.type}</p>
-            </div>
-            <div class="email-form">
-                <h4 class="accordion">Skontaktuj się z nami i wynajmij naczepę już dziś:</h4>
-                <div class="panel">
-                    <form action="mailto:pbtrans@gmail.com" method="post" enctype="text/plain">
-                        <label for="email">Twój email:</label>
-                        <input type="email" id="email" name="email" required>
-                        <label for="temat">Temat:</label>
-                        <input type="text" id="temat" name="temat">
-                        <label for="message">Treść wiadomości:</label>
-                        <textarea id="message" name="message" rows="4" required></textarea>
-                        <button type="submit">Wyślij</button>
-                    </form>
-                </div>
-            </div>
-        `;
-        
-        offersContainer.appendChild(offerElement);
-    });
-});
-
-// Slider functionality
-function changeSlide(n, carouselId) {
-    const carousel = document.getElementById(carouselId);
-    const slides = carousel.getElementsByClassName('slide');
-    let activeIndex = Array.from(slides).findIndex(slide => slide.classList.contains('active'));
-
-    slides[activeIndex].classList.remove('active');
-    activeIndex = (activeIndex + n + slides.length) % slides.length;
-    slides[activeIndex].classList.add('active');
-}
-
-// Update price label dynamically
-function updatePriceLabel(value) {
-    document.getElementById('price-label').textContent = `${value} zł`;
-}
-document.addEventListener('DOMContentLoaded', function() {
-    initializeCarousels();
-    initializeAccordions();
     initializeFilters();
     loadOffers();
 });
@@ -104,49 +38,17 @@ function initializeAccordions() {
     });
 }
 
+function updatePriceLabel(value) {
+    document.getElementById('price-label').textContent = `${value} zł`;
+}
+
 function initializeFilters() {
     const priceRange = document.getElementById('price-range');
-    const priceLabel = document.getElementById('price-label');
     const brandSelect = document.getElementById('brand-select');
     const typeSelect = document.getElementById('type-select');
-    const offersContainer = document.getElementById('offers-container');
+    const yearFromSelect = document.getElementById('year-from-select');
+    const yearToSelect = document.getElementById('year-to-select');
 
-    // Aktualizuj etykietę ceny
-    function updatePriceLabel(value) {
-        priceLabel.textContent = `${value} zł`;
-    }
-
-    // Filtruj oferty
-    function filterOffers() {
-        const selectedPrice = parseInt(priceRange.value);
-        const selectedBrand = brandSelect.value;
-        const selectedType = typeSelect.value;
-        const offers = offersContainer.querySelectorAll('.offer');
-
-        offers.forEach(offer => {
-            const offerPrice = parseInt(offer.getAttribute('data-price'));
-            const offerBrand = offer.getAttribute('data-brand');
-            const offerType = offer.getAttribute('data-type');
-
-            let showOffer = true;
-
-            if (offerPrice > selectedPrice) {
-                showOffer = false;
-            }
-
-            if (selectedBrand !== 'all' && offerBrand !== selectedBrand) {
-                showOffer = false;
-            }
-
-            if (selectedType !== 'all' && offerType !== selectedType) {
-                showOffer = false;
-            }
-
-            offer.style.display = showOffer ? 'block' : 'none';
-        });
-    }
-
-    // Przypisz funkcje do odpowiednich elementów
     priceRange.addEventListener('input', () => {
         updatePriceLabel(priceRange.value);
         filterOffers();
@@ -154,21 +56,66 @@ function initializeFilters() {
 
     brandSelect.addEventListener('change', filterOffers);
     typeSelect.addEventListener('change', filterOffers);
+    yearFromSelect.addEventListener('change', filterOffers);
+    yearToSelect.addEventListener('change', filterOffers);
 
-    // Wywołaj funkcję filtrowania na początku, aby ukryć nieodpowiednie oferty
     filterOffers();
+}
+
+function filterOffers() {
+    const selectedPrice = parseInt(document.getElementById('price-range').value);
+    const selectedBrand = document.getElementById('brand-select').value;
+    const selectedType = document.getElementById('type-select').value;
+    const selectedYearFrom = document.getElementById('year-from-select').value;
+    const selectedYearTo = document.getElementById('year-to-select').value;
+    const offersContainer = document.getElementById('offers-container');
+    const offers = offersContainer.querySelectorAll('.offer');
+
+    offers.forEach(offer => {
+        const offerPrice = parseInt(offer.getAttribute('data-price'));
+        const offerBrand = offer.getAttribute('data-brand');
+        const offerType = offer.getAttribute('data-type');
+        const offerYear = parseInt(offer.getAttribute('data-year'));
+
+        let showOffer = true;
+
+        if (offerPrice > selectedPrice) {
+            showOffer = false;
+        }
+
+        if (selectedBrand !== 'all' && offerBrand !== selectedBrand) {
+            showOffer = false;
+        }
+
+        if (selectedType !== 'all' && offerType !== selectedType) {
+            showOffer = false;
+        }
+
+        if (selectedYearFrom !== 'all' && offerYear < parseInt(selectedYearFrom)) {
+            showOffer = false;
+        }
+
+        if (selectedYearTo !== 'all' && offerYear > parseInt(selectedYearTo)) {
+            showOffer = false;
+        }
+
+        offer.style.display = showOffer ? 'block' : 'none';
+    });
 }
 
 function loadOffers() {
     let offers = JSON.parse(localStorage.getItem('offers')) || [];
     const offersContainer = document.getElementById('offers-container');
     
+    offersContainer.innerHTML = ''; // Ensure offersContainer is empty before adding new offers
+
     offers.forEach((offer, index) => {
         const offerElement = document.createElement('div');
         offerElement.className = 'offer';
         offerElement.setAttribute('data-price', offer.price);
         offerElement.setAttribute('data-brand', offer.brand);
         offerElement.setAttribute('data-type', offer.type);
+        offerElement.setAttribute('data-year', offer.year);
         
         const carouselId = 'carousel' + index;
         
@@ -207,6 +154,6 @@ function loadOffers() {
         offersContainer.appendChild(offerElement);
     });
 
-    // Inicjalizuj ponownie akordeony po dodaniu ofert
+    initializeCarousels();
     initializeAccordions();
 }
